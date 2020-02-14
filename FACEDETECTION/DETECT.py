4649b6cfa9ee03ac791fe594913f0391acd3460b
Shaurabh Tuladhar <shaurabh19@gmail.com>
2020-02-14 14:33:47 +0545

    added

--- FACEDETECTION/DETECT.py
+++ FACEDETECTION/DETECT.py
@@ -22,7 +22,8 @@ while True:
 
                 
     cv2.imshow('img',img)
-    k = cv2.waitKey(30) & 0xff    
+    k = cv2.waitKey(45) & 0xff    
+    k = cv2.waitKey(45) & 0xff    
     if k==27:
       break
 cap.release()
